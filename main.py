1e18efacd6e5c9fb7325cc769df743d2ffaaa9e2
suzukikaho <mugimameawa2828@gmail,com>
2022-07-28 21:17:46 +0900

    ^modify image url

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from skimage.io import imread
 
 
 # download the image
-img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Mount_Fuji_from_Mount_Aino.jpg/640px-Mount_Fuji_from_Mount_Aino.jpg'
+img_url = 'https://pbs.twimg.com/profile_banners/1278607777791750144/1600913901/600x200'
 
 im = imread(img_url)
 
